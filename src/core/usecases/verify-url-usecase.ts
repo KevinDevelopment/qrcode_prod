109cb2cafecb4a1609abce2a9438021605ec9397
KevinDevelopment <kevincristhian@outlook.com>
2024-04-06 15:31:07 -0300

    refactor: refactoring response of serve

--- src/core/usecases/verify-url-usecase.ts
+++ src/core/usecases/verify-url-usecase.ts
@@ -5,23 +5,15 @@ import { UrlToBeChecked } from "../entities/url";
 import { URL } from "../value-objects/url";
 
 export class VerifyUrlUseCase {
-    private urlPort: IUrlPort;
+  private urlPort: IUrlPort;
 
-    constructor(urlPort: IUrlPort) {
-        this.urlPort = urlPort;
-    }
+  constructor(urlPort: IUrlPort) {
+    this.urlPort = urlPort;
+  }
 
-    async perform(input: IUrlInputDto): Promise<IUrlOutPutDto> {
-        try {
-            const url = new UrlToBeChecked(new URL(input.url));
-            const verifyUrl = await this.urlPort.verifyUrl(url.url.value);
-            return verifyUrl;
-        } catch (error) {
-            if (error instanceof Error) {
-                throw new Error(error.message);
-            }
-
-            throw new Error("Erro interno do servidor");
-        }
-    }
+  async perform(input: IUrlInputDto): Promise<IUrlOutPutDto> {
+    const url = new UrlToBeChecked(new URL(input.url));
+    const verifyUrl = await this.urlPort.verifyUrl(url.url.value);
+    return verifyUrl;
+  }
 }

135cf2fbdb230621ac695d082a157d3cbb71321a
KevinDevelopment <kevincristhian@outlook.com>
2024-04-06 12:42:47 -0300

    refactor: refactor server to railway

--- src/presentation/web/express.ts
+++ src/presentation/web/express.ts
@@ -14,8 +14,10 @@ app.use(helmet())
 app.use(cors({ origin: "*" }))
 app.use(router)
 
-const PORT = 3000
+const port: any = process.env.PORT || 3000
+
 app.get("/", (req: Request, res: Response) => res.json("Servidor funcionando normalmente") )
-app.listen(PORT, () => {
-    console.log(`Server running in port ${PORT}`)
+
+app.listen(port, "0.0.0.0", () => {
+    console.log(`Server running in port ${port}`)
 });
